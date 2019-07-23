4a5778224492c87a11940ce4d4afc6f6e04aab77
ericksitocg <ericksitocg@hotmail.com>
2019-07-22 23:41:02 -0500

    agregando listas y metoodos

--- EasyJob.rb
+++ EasyJob.rb
@@ -3,7 +3,7 @@ class Servicio
 	def initialize(nombre,tarifa)
 		@nombre = nombre
 		@tarifa = tarifa
-		@estado = true
+		@estado = false
 	end
 
 	def to_s
@@ -11,6 +11,13 @@ class Servicio
 	end
 
 	#Metodos
+	def get_nombre
+		@nombre
+	end
+
+	def get_tarifa
+		@tarifa
+	end
 
 	def actualizar_servicio
 		puts "actulizando servicio"
@@ -35,7 +42,6 @@ class Usuario
 		@nombre = nombre
 		@usuario = usuario
 		@contrasenia = contrasenia
-
 	end
 
 	def to_s
@@ -43,6 +49,10 @@ class Usuario
 	end
 
 	#metodos
+
+	def get_nombre
+		@nombre
+	end
 
 	def loguear()
 		puts "Logeando"
@@ -59,7 +69,7 @@ end
 
 #clase Cliente
 class Cliente < Usuario
-	def initialize(nombre,usuario,contrasenia,aceptado)
+	def initialize(nombre,usuario,contrasenia)
 		super(nombre,usuario,contrasenia)
 		@aceptado = true
 	end
@@ -79,11 +89,10 @@ class Cliente < Usuario
 end
 #clase Proveedor
 class Proveedor < Usuario
-	servicios = []
-	propuestas = []
-
 	def initialize(nombre,usuario,contrasenia)
 		super(nombre,usuario,contrasenia)
+		@servicios = Array.new
+		@propuestas = Array.new
 	end
 
 	def to_s
@@ -92,15 +101,22 @@ class Proveedor < Usuario
 
 	#Metodos
 
-	def agregar_servicio
-		puts "Agrego un servicio"
+	def agregar_servicio(nombre,tarifa)
+		nuevo_servicio = Servicio.new(nombre,tarifa)
+		@servicios << nuevo_servicio
+		puts "Servicio " + nuevo_servicio.get_nombre + " creado"
 	end
 
-	def configurar_tarifa
-		puts "Configurando la tarifa"
+	def configurar_tarifa(servicio,tarifa)
+		@servicios.each do |servicio_i|
+			servicio_i.get_nombre <=> servicio
+				servicio_i.configurar_tarifa(tarifa)
+				puts "Servicio " + servicio_i.get_nombre + " ahora tiene tarifa " + servicio_i.get_tarifa
+		end
 	end
-
-	def aceptar_propuesta
+##############Cambio
+	def aceptar_propuesta(servicio)
+		servicio.activar_servicio
 		puts "Acpetando la propuesta"
 	end
 
@@ -111,34 +127,61 @@ end
 
 #Clase administrador
 class Administrador < Usuario
-	clientes = []
-	proveedores = []
-
 	def initialize(nombre,usuario,contrasenia)
 		super(nombre,usuario,contrasenia)
+		@clientes = Array.new
+		@proveedores = Array.new
+		@servicios = Array.new
 	end
 
 	#Metodos
-	def crear_usuario()
-		puts "Creando usuario"
+
+	def crear_cliente(nombre,usuario,contrasenia)
+		nuevo_cliente = Cliente.new(nombre,usuario,contrasenia)
+		@clientes <<nuevo_cliente
+		puts "Cliente " + nuevo_cliente.get_nombre + " creado"
 	end
 
-	def actualizar_datos_usuario()
-		puts "Actualizando datos de un usuario especifico"
+	def crear_proveedor(nombre,usuario,contrasenia)
+		nuevo_proveedor = Proveedor.new(nombre,usuario,contrasenia)
+		@proveedores << nuevo_proveedor
+		puts "Proveedor " + nuevo_proveedor.get_nombre + " creado"
 	end
 
-	def desactivar_usuario()
-		puts "Estoy desactivando el usuario"
+	def actualizar_datos_cliente(usuario)
+		puts "Actualizando datos de un cliente especifico " + usuario
 	end
 
-	def generar_reportes()
-		puts "Estoy generando reportes"
+	def actualizar_datos_proveedor(usuario)
+		puts "Actualizando datos de un proveedor especifico " + usuario
 	end
 
-	def crear_servicio()
-		puts "creando un servicio"
+	def desactivar_cliente(usuario)
+		@clientes.each do |cliente_i|
+			cliente_i.get_nombre <=> usuario
+				@clientes.delete(cliente_i)
+				puts "Cliente " + usuario + " desactivado"
+		end
 	end
+
+	def desactivar_proveedor(usuario)
+		@proveedores.each do |proveedor_i|
+			proveedor_i.get_nombre <=> usuario
+				@proveedores.delete(proveedor_i)
+				puts "Proveedor " + usuario + " desactivado"
+		end
+	end
+
+	def crear_servicio(nombre,tarifa)
+		nuevo_servicio = Servicio.new(nombre,tarifa)
+		@servicios << nuevo_servicio
+		puts "Servicio " + nuevo_servicio.get_nombre + " creado"
+	end
+
 end
-
-mi_proveedor = Proveedor.new("AXE","usuario_axe","contrasenia_axe")
-puts mi_proveedor.to_s
+#########################
+administrador = Administrador.new("Erick","user","admin")
+administrador.crear_cliente("Erick","ee","3")
+administrador.crear_proveedor("prove","23","123")
+administrador.desactivar_cliente("Erick")
+administrador.crear_servicio("le",21)
